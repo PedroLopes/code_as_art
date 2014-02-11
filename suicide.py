dc83f9fae49079c770053a7b9e58255dc9759ed7
pedro.lopes <pedro.lopes@hpi.uni-potsdam.de>
2014-02-11 16:16:54 +0100

    suicide

---
+++ suicide.py
@@ -0,0 +1,13 @@
+import os
+from subprocess import call
+
+me = os.getpid()
+call(["kill", str(me)])
+
+print("i live")
+print("\n")
+print("therefore i am")
+
+var = 1
+while var == 1 :
+	"happyness" 
