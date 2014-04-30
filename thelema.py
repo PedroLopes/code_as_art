031faee37f8e61acdaa676e1914597f74d522e57
pedro.lopes <pedro.lopes@hpi.uni-potsdam.de>
2014-04-30 15:15:06 -0400

    thelema now read from file, parsing is incorrect

--- thelema.py
+++ thelema.py
@@ -4,56 +4,19 @@ import re
 
 chapter1_data_split=[]
 
-greetings=[]
-greetings.append("hello")
-greetings.append("hallo")
-greetings.append("hy")
-greetings.append("hi")
-greetings.append("you")
-greetings.append("are")
-greeting = 0
-goodbyes=[]
-goodbyes.append("see")
-goodbyes.append("ya")
-goodbyes.append("you")
-goodbyes.append("thanks")
-goodbyes.append("later")
-goodbyes.append("soon")
-goodbyes.append("out")
-goodbyes.append("away")
-goodbyes.append("disconnect")
-goodbyes.append("logout")
-goodbyes.append("exit")
-goodbyes.append("close")
-goodbyes.append("die")
-goodbye = 0
-doasthouwilts=[] #what should I do? choose? which? 
-doasthouwilts.append("?")
-doasthouwilts.append("should")
-doasthouwilts.append("what")
-doasthouwilts.append("which")
-doasthouwilts.append("do")
-doasthouwilts.append("make")
-doasthouwilts.append("ask")
-doasthouwilts.append("I")
-doasthouwilts.append("will")
-doasthouwilts.append("want")
-doasthouwilts.append("did")
-doasthouwilts.append("that")
-doasthouwilt=0
-loveisthelaws=[] #love is the law, what is love?
-loveisthelaws.append("love")
-loveisthelaw=0
+bookofthelaw_crowley=[] 
+bookofthelaw_parser=[]
 
+#to do
 #take out irrelevant words first
 #cound term frequency
 #check most frequent
 #check if it was also the last one, if yes, random but not much, if twice, yes more random
 
-bookofthelaw="texts/bookofthelaw/bookofthelaw.text"
-chapter1="texts/bookofthelaw/chapter1.text"
-chapter2="texts/bookofthelaw/chapter2.text"
-chapter3="texts/bookofthelaw/chapter3.text"
+bookofthelaw="texts/tests/bookofthelaw_small.text"
+#chapter1="texts/bookofthelaw/chapter1.text" #if you want the complete chapter, see here
+#chapter2="texts/bookofthelaw/chapter2.text" #if you want the complete chapter, see here
+#chapter3="texts/bookofthelaw/chapter3.text" #if you want the complete chapter, see here
 
 if len(sys.argv) != 2:
     print 'Please specify one filename on the command line.'
@@ -62,42 +25,45 @@ if len(sys.argv) != 2:
 with open(bookofthelaw) as f:
     bookofthelaw_data = f.readlines()
 
-print bookofthelaw_data
+#print bookofthelaw_data[1]
 
-#for s in chapter1_data:
-	#print s
-	#t = re.findall("\d+.", s)
-	#my_text = s.replace(t[1], 'a')
-	#print my_text
- 	#t2 = re.search("\d+.", s)
-        #print t2.group(1)
-	#chapter1_data_split.append(t)
-
-#print chapter1_data_split
+crowley=False
+for prophetic_statement in bookofthelaw_data:
+	#prophetic_statement_no_blank_lines = re.findall("-.", prophetic_statement)
+	if (prophetic_statement[0] != '-'):
+		crowley = not crowley #make the code more literary
+		if crowley:
+			print ("mr crowley said:")
+			print prophetic_statement
+			bookofthelaw_crowley.append(prophetic_statement)
+		else:
+			prophetic_statement_parser=prophetic_statement.split(',')
+			print ("we parse it by:")
+			print prophetic_statement_parser
+			bookofthelaw_parser.append(prophetic_statement_parser)
+			
 filename = sys.argv[1]
 body = file(filename, 'rt').read()
 print 'ORIGINAL:', repr(body)
 print
 
+thelemic_power = [0] * len(bookofthelaw_parser)
 print 'TOKENS:'
 lexer = shlex.shlex(body)
+i=-1
 for token in lexer:
-    if token in greetings:
-    	greeting+=1
-    elif token in goodbyes:
-    	goodbye+=1
-    elif token in doasthouwilts:
-    	doasthouwilt+=1
-    elif token in loveisthelaws:
-    	loveisthelaw+=1
-    print repr(token)
-
-#find max here and output it
-
-if doasthouwilt > greeting and doasthouwilt > goodbye:
-	print("do as thou wilt shall be the whole of the law")
-elif greeting > goodbye:
-	print("how are you?")
-elif goodbye > greeting:
-	print("see yah later!")
-#else print("random")
+	print repr(token)
+	i+=1
+	print i
+	for prophetic_statement in bookofthelaw_parser:
+		print "this is to match"
+		print prophetic_statement
+		#token_essence  = re.search("\d+.", prophetic_statement) #we can't really do this because its a match 1:1 now
+		for thelemic_token in prophetic_statement:
+			print "we are now looking at the small token"
+			print thelemic_token
+			if (token == thelemic_token):
+				print ("match---------------------------------------------------------------------")
+				thelemic_power[i]+=1
+					
+print thelemic_power			
