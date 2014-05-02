8b7ea9fb7e04d8f6619e92378c59ee7fbdeb24d6
pedro.lopes <pedro.lopes@hpi.uni-potsdam.de>
2014-05-02 16:05:19 -0400

    bruno realized that the print statements were not python3 ready :D thanks bruno

--- thelema.py
+++ thelema.py
@@ -7,7 +7,6 @@ bookofthelaw_crowley=[]
 bookofthelaw_parser=[]
 
 #to do
-#make case insensitive
 #count the words that appear inside the sentence thelema*2, and the sentence parser itself * factor of size
 #cound term frequency -- done
 #check most frequent
@@ -25,13 +24,13 @@ for prophetic_statement in bookofthelaw_data:
 	if (prophetic_statement[0] != '-'):
 		crowley = not crowley 
 		if crowley:
-			#print ("mr crowley said:")
-			#print prophetic_statement
+			#print("mr crowley said:")
+			#print(prophetic_statement)
 			bookofthelaw_crowley.append(prophetic_statement)
 		else:
 			prophetic_statement_parser=prophetic_statement.split(',')
-			#print ("we parse it by:")
-			#print prophetic_statement_parser
+			#print("we parse it by:")
+			#print(prophetic_statement_parser)
 			bookofthelaw_parser.append(prophetic_statement_parser)
 
 while True:
@@ -40,33 +39,40 @@ while True:
 
 #this is if you want to read your will from a file			
 #if len(sys.argv) != 2:
-    #print 'Please specify one filename on the command line.'
+    #print('Please specify one filename on the command line.')
 #    sys.exit(1)
 #filename = sys.argv[1]
 #yourwill = file(filename, 'rt').read()
-#print 'ORIGINAL:', repr(yourwill)
-#print
+#print('ORIGINAL:', repr(yourwill))
 
 	thelemic_power = [0] * len(bookofthelaw_parser)
-	#print 'TOKENS:'
+	#print('TOKENS:')
 	dissecting_yourwill = shlex.shlex(yourwill)
 	for will in dissecting_yourwill:
-		#print repr(will)
+		#print(repr(will))
 		i=-1
 		for prophetic_statement in bookofthelaw_parser:
 			i+=1
-			#print i
-			#print "this is to match"
-			#print prophetic_statement
+			#print("this is to match")
+			#print(prophetic_statement)
 			#token_essence  = re.search("\d+.", prophetic_statement) #we can't really do this because its a match 1:1 now
 			for thelemic_will in prophetic_statement:
-				#print "we are now looking at the small token"
-				#print thelemic_token
-				if (will == thelemic_will):
-					#print ("match---------------------------------------------------------------------")
+				#print("we are now looking at the small token")
+				#print(thelemic_token)
+				if (will.lower() == thelemic_will.lower()):
+					#print("match---------------------------------------------------------------------")
 					thelemic_power[i]+=1
 					
-	#print thelemic_power	
+	#print(thelemic_power)
+
+	#iterate over array and divide by lenght of setence
+	#e.g., user says: i dont understand thelema (word count 3)
+        #and thelema#1 is: thelema is the law (word count 4 vs user count 4;  = 1)
+        #and thelema key#1 is: i don't understand (word count 3 vs user count 4; 4/3 = 1)
+        #and thelema#2 is: love is the law, lover over will (word count 7 vs user count 4 = 0.33)
+        #and thelema key#2 is: i don't understand why the match is not perfect (word count 9 vs user count 4 4/9 = 0.33)
+        
+	
 	maximal_frequency = 0
 	maximal_index = -1
 	resonant_index=-1
@@ -75,12 +81,12 @@ while True:
 		if (resonant_frequency > maximal_frequency):
 			maximal_frequency = resonant_frequency
 			maximal_index = resonant_index
-	#print maximal_index
+	#print(maximal_index)
 	if (maximal_index == -1 or maximal_index == 0):
 		the_answer = randint(0,len(bookofthelaw_crowley)-2)
 	else:	
 		the_answer = maximal_index
-	print bookofthelaw_crowley[the_answer]
+	print(bookofthelaw_crowley[the_answer])
 	if (maximal_index == len(bookofthelaw_crowley)-1):
 		sys.exit()
-	#print bookofthelaw_crowley
+	#print(bookofthelaw_crowley)
